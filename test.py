8b186509b377605cf087eae0b482b042d304c0fc
Carlo <c.mantovani@edu.pucrs.br>
2023-09-16 17:01:37 -0300

    fix: gauss-jacobi

--- test.py
+++ test.py
@@ -1,61 +1,56 @@
 import numpy as np
 
-#TODO: verificar convergencia do metodo de gauss-jacobi aplicando coeficientes as equacoes e verificando proximidade com termos independentes
+# Function to calculate the sum of a row in the coefficient matrix
+def row_sum(coefficient_matrix, independent_terms, row, current_x):
+    sum_result = 0
+    for j in range(len(coefficient_matrix)):
+        sum_result += coefficient_matrix[row, j] * current_x[j]
+    return sum_result
 
-def verifica_convergencia_gauss_jacobi(matriz, coeficientes, termos_independentes):
-    n = len(matriz)
-    x_proximo = np.zeros(n)
-    # creates equations and apply coeficients to equations to verify convergence by checking proximity to independent terms
+# Function to check the convergence of Gauss-Jacobi method
+def check_convergence_gauss_jacobi(coefficient_matrix, calculated_x, independent_terms, tolerance=1e-6):
+    n = len(coefficient_matrix)
+
     for i in range(n):
-        soma = 0
-        for j in range(n):
-            if j != i:
-                soma += matriz[i, j] * coeficientes[j]
-        x_proximo[i] = (termos_independentes[i] - soma) / matriz[i, i]
-
-    if np.allclose(x_proximo, coeficientes, rtol=1e-6, atol=1e-6):
-        return False
+        row_sum_result = row_sum(coefficient_matrix, independent_terms, i, calculated_x)
+        difference = abs(row_sum_result - independent_terms[i])
+        if difference > tolerance:
+            return False
     return True
 
-def gauss_jacobi(matriz_coeficientes, vetor_termos_independentes, max_iteracoes=100, tolerancia=1e-6):
-    n = len(matriz_coeficientes)
-    #chute inicial 2, 3, 2, 6 = np.array([2, 3, 2, 6])
-    x_atual = np.ones(n)
-    x_proximo = np.zeros(n)
+# Function to solve a system of linear equations using Gauss-Jacobi method
+def gauss_jacobi(coefficient_matrix, independent_terms, max_iterations=100, tolerance=1e-6):
+    n = len(coefficient_matrix)
 
-    for iteracao in range(max_iteracoes):
-        # alcance do numero de equacoes
+    current_x = np.ones(n)
+    next_x = np.zeros(n)
+
+    for iteration in range(max_iterations):
         for i in range(n):
-            soma = 0
+            sum_result = 0
             for j in range(n):
                 if j != i:
-                    soma += matriz_coeficientes[i, j] * x_atual[j]
-            x_proximo[i] = (vetor_termos_independentes[i] - soma) / matriz_coeficientes[i, i]
-        x_atual[:] = x_proximo
-        #apply coeficients to equations to verify convergence
-        if not verifica_convergencia_gauss_jacobi(matriz_coeficientes, x_atual, vetor_termos_independentes):
-            return x_atual, iteracao + 1
-        
-      
-        print (x_proximo)
+                    sum_result += coefficient_matrix[i, j] * current_x[j]
+            next_x[i] = (independent_terms[i] - sum_result) / coefficient_matrix[i, i]
+        current_x[:] = next_x
+        if check_convergence_gauss_jacobi(coefficient_matrix, current_x, independent_terms):
+            return current_x, iteration + 1
+    return None, max_iterations  # Returns None if convergence is not achieved
 
-    return None, max_iteracoes  # Retorna None se a convergência não for alcançada
-
-# Exemplo de uso
+# Example of usage
 if __name__ == "__main__":
-    #matriz_coeficientes = np.array([[6, 2, 3, -1],
-    #                                [4, 2, 4, 7],
-    #                                [8, 3, 5 , -9],
-    #                                [-4, 2, 5, 6]])
-    matriz_coeficientes = np.array([[9,6,5],[6,8,4],[2,1,7]])
+    coefficient_matrix = np.array([[12, 6, 5],
+                                   [1, 8, 4],
+                                   [2, 1, 7]])
 
-    vetor_termos_independentes = np.array([-1,6,5])
+    independent_terms = np.array([-1, 6, 5])
 
-
-    solucao, iteracoes = gauss_jacobi(matriz_coeficientes, vetor_termos_independentes)
-    if solucao is not None:
-        print("Solução encontrada após", iteracoes, "iterações:")
-        print(solucao)
+    solution, iterations = gauss_jacobi(coefficient_matrix, independent_terms)
+    if solution is not None:
+        print("Solution found after", iterations, "iterations:")
+        print(solution)
+        print ("Checking solution:")
+        print (" Original Independent Terms: ", independent_terms)
+        print(" Obtained Values using the Solution: ", np.dot(coefficient_matrix, solution))
     else:
-        print("O método de Gauss-Jacobi não convergiu após", iteracoes, "iterações.")
-    
+        print("Gauss-Jacobi method did not converge after", iterations, "iterations.")
